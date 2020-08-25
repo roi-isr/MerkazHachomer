00e731b4f6521225f78902884507fea4d6ea9266
Roi Israeli <roiisra4@gmail.com>
2020-08-25 20:07:34 +0300

    virtual store update

--- src/project-files/roi-folder/SearchForm.jsx
+++ src/project-files/roi-folder/SearchForm.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+import "./SearchForm.css"
+import logo from "./static-files/logo.JPG"
 
-class Credit extends Component {
+
+const SearchInput = () => {
+    return (
+        <form className="vs-search">
+            <input
+                placeholder="חפש מוצר לפי מילות מפתח"
+                dir="rtl"
+                className="form-control search-input w-50"
+                type="text" />
+            <button style={{ "margin-left": "5px" }} dir="rtl" className="btn btn-outline-warning">חפש!</button>
+            <button type="button" dir="rtl" className="btn btn-outline-warning">חיפוש מתקדם</button>
+        </form>
+    );
+}
+
+
+class SearchForm extends Component {
 
     render() {
         return (
-            <div></div>
+            <div>
+                <header className="vs-header">
+                    <div className="vs-header-div">
+                        <div className="vs-right-header">
+                            <h1 className="vs-welcome-title">ברוכים הבאים לחנות הוירטואלית</h1>
+                            <SearchInput />
+                        </div>
+                        <img className="vs-logo" src={logo} alt="logo" />
+
+                    </div>
+                </header>
+            </div>
         );
     }
 }
-export default Credit;
+export default SearchForm;
--- src/project-files/roi-folder/VirtualStore.jsx
+++ src/project-files/roi-folder/VirtualStore.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-
+import SearchForm from "./SearchForm"
 class VirtualStore extends Component {
 
     render() {
         return (
-            <div></div>
+            <div>
+                <SearchForm/>
+            </div>
         );
     }
 }
